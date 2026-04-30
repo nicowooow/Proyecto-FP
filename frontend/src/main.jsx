c63160a95a84ca781b835ec35ab406cfa8b80801
nicowooow <nicolasgianfranco123@gmail.com>
2026-04-30 02:08:19 +0200

    modificamos la parte de los SEO para que no tenga repeticiones innecesarias

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -37,38 +37,25 @@ function AppWithHeader() {
 	const hideHeader = /^\/[yY]our[tT]ree\/[^/]+$/.test(location.pathname);
 	const titles = {
 		'/': 'YourTree - Link in Bio & Community',
-		'/forums': 'YourTree Forums',
-		'/signin': 'Sign In - YourTree',
-		'/signup': 'Sign Up - YourTree',
-		'/profile/:username': 'Profile - YourTree'
+		'/Forums': 'YourTree Forums',
+		'/Signin': 'Sign In - YourTree',
+		'/Signup': 'Sign Up - YourTree',
+		'/Profile/:username': 'Profile - YourTree'
 	};
 
 	const descriptions = {
 		'/': 'YourTree is a link in bio & community platform. Share your profile, connect with others, and build your online presence.',
-		'/forums': 'Join YourTree forums to discuss and engage with our community members.',
-		'/signin': 'Sign in to your YourTree account to access your profile and community features.',
-		'/signup': 'Create your YourTree account and start sharing your profile with the world.',
-		'/profile/:username': 'Check out this user profile on YourTree.'
+		'/Forums': 'Join YourTree forums to discuss and engage with our community members.',
+		'/Sign_in': 'Sign in to your YourTree account to access your profile and community features.',
+		'/Sign_up': 'Create your YourTree account and start sharing your profile with the world.',
+		'/Profile/:username': 'Check out this user profile on YourTree.'
 	};
 
 	return (
 		<>
 			<Helmet>
 				<html lang="en" />
-				<title>{titles[location.pathname] || 'YourTree'}</title>
-				<meta name="description" content={descriptions[location.pathname] || 'YourTree - Your link in bio & community platform'} />
 				<meta name="theme-color" content="#ffffff" />
-				<meta property="og:title" content={titles[location.pathname] || 'YourTree'} />
-				<meta property="og:description" content={descriptions[location.pathname] || 'YourTree - Your link in bio & community platform'} />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content={`https://demo.treedlink.com${location.pathname}`} />
-				<meta property="og:image" content="https://demo.treedlink.com/web-app-manifest-512x512.png" />
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content={titles[location.pathname] || 'YourTree'} />
-				<meta name="twitter:description" content={descriptions[location.pathname] || 'YourTree - Your link in bio & community platform'} />
-				<meta name="twitter:image" content="https://demo.treedlink.com/web-app-manifest-512x512.png" />
-				<link rel="canonical" href={`https://demo.treedlink.com${location.pathname}`} />
-				<meta name="robots" content="index, follow" />
 			</Helmet>
 			<AnalyticsTracker />
 			{!hideHeader && <Header />}
@@ -80,26 +67,28 @@ function AppWithHeader() {
 					alignItems: "center",
 					justifyContent: "center",
 				}}>Cargando...</div >
-			}></Suspense >
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/YourTree" element={<YourTree />} />
-				<Route path="/YourTree/:username" element={<YourTreeUser />} />
-				<Route path="/" element={<Home />} />
-				<Route path="/Forums" element={<Forums />} />
-				<Route path="/Forums/:forumId" element={<Forums />} />
-				<Route path="/Recent_Pages" element={<Recent_Pages />} />
-				<Route path="/Profile" element={<Profile />} />
-				{/* <Route path="/profile/:username" element={<Profile />} /> */}
-				<Route path="/Log_out" element={<Log_out />} />
-				<Route path="/Sign_in" element={<Sign_in />} />
-				<Route path="/Sign_up" element={<Sign_up />} />
-				<Route path="/Forgot_Password" element={<Forgot_Password />} />
-				<Route path="/Reset_Password" element={<Reset_Password />} />
-				<Route path="/*" element={<NotFound />} />
-				<Route path="/404" element={<NotFound />} />
-			</Routes>
-			<BuyMeACoffee />
+			}>
+
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/YourTree" element={<YourTree />} />
+					<Route path="/YourTree/:username" element={<YourTreeUser />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/Forums" element={<Forums />} />
+					<Route path="/Forums/:forumId" element={<Forums />} />
+					<Route path="/Recent_Pages" element={<Recent_Pages />} />
+					<Route path="/Profile" element={<Profile />} />
+					{/* <Route path="/profile/:username" element={<Profile />} /> */}
+					<Route path="/Log_out" element={<Log_out />} />
+					<Route path="/Sign_in" element={<Sign_in />} />
+					<Route path="/Sign_up" element={<Sign_up />} />
+					<Route path="/Forgot_Password" element={<Forgot_Password />} />
+					<Route path="/Reset_Password" element={<Reset_Password />} />
+					<Route path="/*" element={<NotFound />} />
+					<Route path="/404" element={<NotFound />} />
+				</Routes>
+				<BuyMeACoffee />
+			</Suspense >
 		</>
 	);
 }
